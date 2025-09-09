6fc886a175686baedba21bbbf9bb26e1948c7e9d
vinayakarjunoffical <vinayak.arjun@goelectronix.com>
2025-09-09 17:07:01 +0530

    add new code

--- middleware.js
+++ middleware.js
@@ -1,37 +1,37 @@
-import { NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
+// import { NextResponse } from "next/server";
+// import jwt from "jsonwebtoken";
 
-const SECRET = process.env.JWT_SECRET || "supersecretkey";
+// const SECRET = process.env.JWT_SECRET || "supersecretkey";
 
-export async function middleware(req) {
-  const token = req.cookies.get("accessToken")?.value;
+// export async function middleware(req) {
+//   const token = req.cookies.get("accessToken")?.value;
 
-  
+//   console.log("token ",token)
 
-  if (!token) {
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
+//   if (!token) {
+//     return NextResponse.redirect(new URL("/login", req.url));
+//   }
 
-  try {
-    const decoded = jwt.verify(token, SECRET);
+//   try {
+//     const decoded = jwt.verify(token, SECRET);
 
-    const { pathname } = req.nextUrl;
+//     const { pathname } = req.nextUrl;
 
-    // Role-based redirect
-    if (pathname.startsWith("/dashboard/admin") && decoded.role !== "admin") {
-      return NextResponse.redirect(new URL("/dashboard/user", req.url));
-    }
-    if (pathname.startsWith("/dashboard/manager") && decoded.role !== "manager") {
-      return NextResponse.redirect(new URL("/dashboard/user", req.url));
-    }
+//     // Role-based redirect
+//     if (pathname.startsWith("/dashboard/admin") && decoded.role !== "admin") {
+//       return NextResponse.redirect(new URL("/dashboard/user", req.url));
+//     }
+//     if (pathname.startsWith("/dashboard/manager") && decoded.role !== "manager") {
+//       return NextResponse.redirect(new URL("/dashboard/user", req.url));
+//     }
 
-    return NextResponse.next();
-  } catch (err) {
-    // Try refreshing token
-    return NextResponse.redirect(new URL("/login", req.url));
-  }
-}
+//     return NextResponse.next();
+//   } catch (err) {
+//     // Try refreshing token
+//     return NextResponse.redirect(new URL("/login", req.url));
+//   }
+// }
 
-export const config = {
-  matcher: ["/dashboard/:path*"], // Protect all dashboard routes
-};
+// export const config = {
+//   matcher: ["/dashboard/:path*"], // Protect all dashboard routes
+// };
